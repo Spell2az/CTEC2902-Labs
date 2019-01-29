b6e24b2e1625c704080bfa2316a5cf7d88abdef5
Bin-A <Bin-A@LAPTOP-2G2915GD>
2019-01-29 10:43:34 +0000

    Abdullah

--- LectureExample/LectureExample/Program.cs
+++ LectureExample/LectureExample/Program.cs
@@ -15,6 +15,14 @@ namespace LectureExample
             Console.WriteLine("Hello World!");
             Console.ReadKey();
 
+
+
+
+
+            //Abdullah 
+
+        
+        
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
     }
